b0089138a22b6d8bfb2b7c88742eda49409667f7
Harshitaa25 <harshitamahakalkar@gmail.com>
2025-08-24 18:39:45 +0530

    database chnages 2

--- db/db.js
+++ db/db.js
@@ -1,2 +1,7 @@
 console.log("Database connection established");
 console.log("Database connection closed");
+
+console.log("Database query executed");
+
+console.log("Database error occurred"); 
+console.log("Database disconnection initiated");
